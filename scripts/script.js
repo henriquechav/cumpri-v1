cc57dd3a71e65f6b4f9d9e590a9560852403b692
Henrique Chaves <henriquelopes0611@gmail.com>
2024-06-01 21:56:32 -0300

    add collapse sidebar

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
+/*  HEADER MENU  */
 
+const headerMenu = document.querySelector(".header-menu");
+const sidebar = document.querySelector(".main-container > aside");
+
+console.log(sidebar);
+
+headerMenu.addEventListener("click", (e) => {
+  sidebar.classList.toggle("hidden");
+});
