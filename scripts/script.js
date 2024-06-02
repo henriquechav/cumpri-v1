e4605d1332da15c6f988242d3db85d55d7b49cf5
Henrique Chaves <henriquelopes0611@gmail.com>
2024-06-02 13:24:58 -0300

    code refactoring

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,25 @@
-/*  HEADER MENU  */
+/*  HEADER MENU BUTTON  */
 
-const headerMenu = document.querySelector(".header-menu");
-const sidebar = document.querySelector(".main-container > aside");
+const headerMenu = document.querySelector("#header-menu-icon");
+const sidebar = document.querySelector("#app-sidebar");
 
 console.log(sidebar);
 
 headerMenu.addEventListener("click", (e) => {
   sidebar.classList.toggle("hidden");
+});
+
+/*  FRAME MODAL  */
+
+const createFrameModal = document.querySelector("#create-frame-modal");
+const createFrameBtn = document.querySelector("#create-frame-button");
+
+createFrameBtn.addEventListener("click", (e) => {
+  createFrameModal.showModal();
+});
+
+const cancelModal = document.querySelector("#create-frame-cancel");
+
+cancelModal.addEventListener("click", (e) => {
+  createFrameModal.close();
 });
