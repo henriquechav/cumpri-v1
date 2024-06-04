9067a7c32f7994d23c2d61ba3b161e073f256af2
Henrique Chaves <henriquelopes0611@gmail.com>
2024-06-04 10:26:50 -0300

    create `TasksAPI.js` and define its scope

--- scripts/TasksAPI.js
+++ scripts/TasksAPI.js
@@ -1,30 +1,77 @@
+/**  
+ *   TasksAPI works like a Model (from MVC pattern) in my project.
+ *   
+ *   For now, it dialogues with browser localStorage to persist data.
+ *   
+ *   It receives titles and tasks status from the user, and manages
+ *   IDs and taskSet percentage (our business logic).  
+ * 
+ */
+
 export default class TasksAPI {
-    getData() {
-        return [{
-            frameId: 1,
-            frameTitle: "Tarefas",
-            taskSetList: [{
-                taskSetId: 1,
-                taskSetTitle: "Tarefas do Dia",
-                percentage: 33,
-                taskList: [{
-                    taskId: 1,
-                    text: "Fazer faxina",
-                    status: "done"
-                }, {
-                    taskId: 2,
-                    text: "Ler a Ilíada",
-                    status: "waiting"
-                }]
-            }]
-        }];
+    static getData() {
+        const data = JSON.parse(localStorage.getItem("mytasks-data") || "[]");
+
+        return data;
     }
 
-    saveData() {
+    static createFrame(frameToCreate) {
+        const frameList = TasksAPI.getData();
+
+        frameToCreate.taskSetList = [];
+
+        // create id of new frame
+        const amountOfFrames = frameList.length;
+        frameToCreate.frameId = amountOfFrames > 0 ? (frameList[amountOfFrames-1].frameId + 1) : 1;
+
+        frameList.push(frameToCreate);
+
+        localStorage.setItem("mytasks-data", JSON.stringify(frameList));
+    }
+
+    static createTaskSet(frameId, taskSetToCreate) { 
+        const frameList = TasksAPI.getData();
+        const frameParent = frameList.find(frame => frame.frameId == frameId);
+        const taskSetList = frameParent.taskSetList;
+
+        // create id for new taskSet
+        const amountOfTaskSet = taskSetList.length;
+        taskSetToCreate.taskSetId = amountOfTaskSet > 0 ? (taskSetList[amountOfTaskSet-1].taskSetId + 1) : 1;
+    
+        taskSetToCreate.percentage = 0;
+
+        const taskList = taskSetToCreate.taskList;
+        // create id for each task
+        for (let i = 0; i < taskList.length; i++) {
+            taskList[i].taskId = i + 1;
+        }
+
+        frameParent.taskSetList.push(taskSetToCreate);
+
+        localStorage.setItem("mytasks-data", JSON.stringify(frameList));
+    }
+
+    static updateFrameTitle(frameToUpdate) {
+        const frameList = TasksAPI.getData();
+        const existing = frameList.find(frame => frame.frameId == frameToUpdate.frameId);
+
+        existing.frameTitle = frameToUpdate.frameTitle;
+
+        localStorage.setItem("mytasks-data", JSON.stringify(frameList));
+    }
+
+    static updateTaskSet() {
 
     }
 
-    deleteData() {
+    static deleteFrame(frameId) {
+        const frameList = TasksAPI.getData();
+        const newFrameList = frameList.filter(frame => frame.frameId != frameId);
+
+        localStorage.setItem("mytasks-data", JSON.stringify(newFrameList));
+    }
+
+    static deleteTaskSet(taskSetId) {
 
     }
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,24 @@
 import TasksAPI from "./TasksAPI.js";
 
+    
 
+console.log(TasksAPI.getData());
+
+// [{
+//     frameId: 1,
+//     frameTitle: "Tarefas",
+//     taskSetList: [{
+//         taskSetId: 1,
+//         taskSetTitle: "Tarefas do Dia",
+//         percentage: 33,
+//         taskList: [{
+//             taskId: 1,
+//             text: "Fazer faxina",
+//             status: "done"
+//         }, {
+//             taskId: 2,
+//             text: "Ler a Ilíada",
+//             status: "waiting"
+//         }]
+//     }]
+// }];
