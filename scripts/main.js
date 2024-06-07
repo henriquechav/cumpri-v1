22080b82fffc46b8b6850d97b7c413f5e20e60ff
Henrique Chaves <henriquelopes0611@gmail.com>
2024-06-07 10:30:42 -0300

    adjust user and drag icons details

--- scripts/main.js
+++ scripts/main.js
@@ -7,5 +7,10 @@ headerMenu.addEventListener("click", () => {
     sidebar.classList.toggle("sidebar--hidden");
 });
 
+const userIcon = document.querySelector(".header__user-icon");
+userIcon.addEventListener("click", () => {
+    alert("As funcionalidades de usuário ainda não foram implementadas.");
+});
+
 const root = document.querySelector("#root");
 const app = new App(root);
